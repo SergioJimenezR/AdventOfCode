19e83bc2480721f9fd1a2d0a28e195db3790e10c
Sergio Jiménez Roncero <sergiojimenezroncero@gmail.com>
2022-12-03 18:22:36 +0100

    Día 2 completado.

---
+++ aoc2022/src/main/java/sergiojimenezr/aoc2022/day2/MainParte1.java
@@ -0,0 +1,31 @@
+package sergiojimenezr.aoc2022.day2;
+
+import java.io.FileNotFoundException;
+
+import java.util.Arrays;
+import java.util.List;
+
+import sergiojimenezr.utilities.utils.Lector;
+
+public class MainParte1 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		List<String> lista = Lector.leerArchivo("../aoc2022/src/main/java/sergiojimenezr/aoc2022/day2/input.txt");
+
+		List<String> listJugadas = Arrays.asList("ZXY", "XYZ", "YZX");
+
+		int sumatorio = 0;
+
+		for (String strJugada : lista) {
+			char eleccionOponente = strJugada.charAt(0);
+			char miEleccion = strJugada.charAt(2);
+			String planteamiento = listJugadas.get(eleccionOponente - 'A');
+			sumatorio += planteamiento.indexOf(miEleccion) * 3;
+			sumatorio += miEleccion - 'X' + 1;
+		}
+		System.out.println(sumatorio);
+
+	}
+
+}
---
+++ aoc2022/src/main/java/sergiojimenezr/aoc2022/day2/MainParte2.java
@@ -0,0 +1,34 @@
+package sergiojimenezr.aoc2022.day2;
+
+import java.io.FileNotFoundException;
+
+import java.util.Arrays;
+import java.util.List;
+
+import sergiojimenezr.utilities.utils.Lector;
+
+public class MainParte2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		List<String> lista = Lector.leerArchivo("../aoc2022/src/main/java/sergiojimenezr/aoc2022/day2/input.txt");
+
+		List<String> listJugadas = Arrays.asList("ZXY", "XYZ", "YZX");
+
+		int sumatorio = 0;
+		for (String strJugada : lista) {
+
+			char eleccionOponente = strJugada.charAt(0);
+			String planteamiento = listJugadas.get(eleccionOponente - 'A');
+
+			char debidoResultado = strJugada.charAt(2);
+			char miDebidaJugada = planteamiento.charAt(debidoResultado - 'X');
+
+			sumatorio += listJugadas.get(strJugada.charAt(0) - 'A').indexOf(miDebidaJugada) * 3;
+			sumatorio += miDebidaJugada - 'X' + 1;
+		}
+		System.out.println(sumatorio);
+
+	}
+
+}
