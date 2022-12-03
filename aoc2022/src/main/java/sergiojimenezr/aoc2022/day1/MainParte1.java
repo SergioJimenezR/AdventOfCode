52649ebc517e2a5551661aa90ed11ab31db1f28b
Sergio Jiménez Roncero <sergiojimenezroncero@gmail.com>
2022-12-03 18:13:40 +0100

    Inicialización.

---
+++ aoc2022/src/main/java/sergiojimenezr/aoc2022/day1/MainParte1.java
@@ -0,0 +1,15 @@
+package sergiojimenezr.aoc2022.day1;
+
+import java.io.FileNotFoundException;
+
+import sergiojimenezr.utilities.utils.Lector;
+
+public class MainParte1 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		Lector.leerArchivo("../aoc2022/src/main/java/sergiojimenezr/aoc2022/day1/input.txt");
+
+	}
+
+}
---
+++ utilities/src/main/java/sergiojimenezr/utilities/utils/Lector.java
@@ -0,0 +1,24 @@
+package sergiojimenezr.utilities.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lector {
+
+	private Lector() {
+
+	}
+
+	public static List<String> leerArchivo(String ruta) throws FileNotFoundException {
+		List<String> lectura = new ArrayList<>();
+		Scanner sc = new Scanner(new File(ruta));
+		while (sc.hasNextLine())
+			lectura.add(sc.nextLine());
+		sc.close();
+		return lectura;
+	}
+
+}
