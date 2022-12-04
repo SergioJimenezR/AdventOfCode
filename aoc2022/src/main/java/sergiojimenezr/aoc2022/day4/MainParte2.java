9466ee1006a0772cc3935628b6aa0d6a4610fd40
Sergio Jiménez Roncero <sergiojimenezroncero@gmail.com>
2022-12-04 16:22:34 +0100

    Día 4 completado.

---
+++ aoc2022/src/main/java/sergiojimenezr/aoc2022/day4/MainParte2.java
@@ -0,0 +1,28 @@
+package sergiojimenezr.aoc2022.day4;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import sergiojimenezr.utilities.utils.Lector;
+
+public class MainParte2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		List<String> lista = Lector.leerArchivo("../aoc2022/src/main/java/sergiojimenezr/aoc2022/day4/input.txt");
+
+		int contador = 0;
+		for (String linea : lista) {
+			int[] par1 = new int[] { Integer.parseInt(linea.split(",")[0].split("-")[0]),
+					Integer.parseInt(linea.split(",")[0].split("-")[1]) };
+			int[] par2 = new int[] { Integer.parseInt(linea.split(",")[1].split("-")[0]),
+					Integer.parseInt(linea.split(",")[1].split("-")[1]) };
+
+			if (par1[0] <= par2[1] && par1[1] >= par2[0])
+				contador++;
+		}
+		System.out.println(contador);
+
+	}
+
+}
